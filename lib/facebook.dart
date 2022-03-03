733172bb41cc46fc62fb9dccf9ade15caaaa802a
Muhammad-Ullah <muhammad.mail1625@gmail.com>
2022-03-03 23:59:07 +0500

    facebook login added

--- lib/facebook.dart
+++ lib/facebook.dart
@@ -1,65 +1,55 @@
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:flutter/material.dart';
-//
-//
-//
-// class FacebookLogin extends StatefulWidget {
-//   const FacebookLogin({Key? key}) : super(key: key);
-//
-//   @override
-//   _FacebookLoginState createState() => _FacebookLoginState();
-// }
-//
-// class _FacebookLoginState extends State<FacebookLogin> {
-//
-//   Map<String, dynamic>? _userData;
-//
-//   String welcome = "Facebook";
-//
-//   @override
-//   void initState() {
-//     // TODO: implement initState
-//     super.initState();
-//
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: Column(
-//         mainAxisAlignment: MainAxisAlignment.center,
-//         children: [
-//           TextButton(onPressed: (){
-//             signInWithFacebook();
-//           }, child: Text(welcome))
-//
-//         ],
-//       ),
-//     );
-//   }
-//
-//
-//   Future<UserCredential> signInWithFacebook() async {
-//
-//     final LoginResult result = await FacebookAuth.instance.login(permissions:['email']);
-//
-//
-//     if (result.status == LoginStatus.success) {
-//
-//       final userData = await FacebookAuth.instance.getUserData();
-//       _userData = userData;
-//     } else {
-//       print(result.message);
-//     }
-//
-//     setState(() {
-//       welcome = _userData?['email'];
-//     });
-//
-//
-//     final OAuthCredential facebookAuthCredential = FacebookAuthProvider.credential(result.accessToken!.token);
-//
-//     return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-//   }
-//
-// }
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+
+
+class FacebookLogin extends StatefulWidget {
+  const FacebookLogin({Key? key}) : super(key: key);
+
+  @override
+  _FacebookLoginState createState() => _FacebookLoginState();
+}
+
+class _FacebookLoginState extends State<FacebookLogin> {
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Facebook Login",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        body: Center(
+          child:   _displayLoginButton(),
+        ),
+    );
+  }
+
+
+  _displayLoginButton() {
+    return TextButton(
+      child: const Text(
+        "Login with Facebook",
+        style: TextStyle(color: Colors.blue),
+      ),
+      onPressed: () {
+        signInWithFacebook();
+      },
+    );
+  }
+  void signInWithFacebook() async {
+    try {
+      final LoginResult result = await FacebookAuth.instance.login();
+      print("Success");
+      final AuthCredential facebookCredential = FacebookAuthProvider.credential(result.accessToken!.token);
+      final userCredential = await FirebaseAuth.instance.signInWithCredential(facebookCredential);
+
+    } catch (e) {
+      print("error occured");
+      print(e.toString());
+    }
+  }
+
+}
